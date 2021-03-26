61547da7940a85c3c7952d5b5f1ae5fab07db75b
Ayrton Herrera <Arherzu@gmail.com>
2021-03-25 19:37:38 -0500

    se creo el proyecto compras

---
+++ appcompras/Models/Producto.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace appcompras.Models
+{
+    public class Producto
+    {
+       public String nombre {get; set;}
+       public double precio {get; set;}
+       public int cantidad {get; set;}
+    }
+}
